b690ee19eadb186c3a3dbe368bc0f9fc0ce808a4
Allen Sanders <asanders@gamuttechnologysvcs.com>
2024-11-27 16:31:52 -0500

    fixed switch

--- week02/homework/week02-solutions/src/main/java/expeditors/week02/solutions/task03/Grades.java
+++ week02/homework/week02-solutions/src/main/java/expeditors/week02/solutions/task03/Grades.java
@@ -14,10 +14,10 @@ public class Grades {
                     "Invalid value provided for test score - please try again",
                     -1, 100);
             var gradeLetter = switch (grade) {
-                case Integer g when grade >= 90 -> "A";
-                case Integer g when grade >= 80 -> "B";
-                case Integer g when grade >= 70 -> "C";
-                case Integer g when grade >= 60 -> "D";
+                case Integer g when g >= 90 -> "A";
+                case Integer g when g >= 80 -> "B";
+                case Integer g when g >= 70 -> "C";
+                case Integer g when g >= 60 -> "D";
                 default -> "F";
             };
             System.out.printf("Your letter grade is a %s\n", gradeLetter);
